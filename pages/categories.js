d9783c43f953b064e549f874b7e0a9cf0f29fcd0
77001 <alimzhanzhakashev@gmail.com>
2023-10-19 23:04:06 +0600

    init commit add categories

--- pages/categories.js
+++ pages/categories.js
@@ -1,14 +1,24 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Categories() {
   const [name, setName] = useState("");
-
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  function fetchCategories() {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post("/api/categories", { name });
+    await axios.post("/api/categories", { name, parentCategory });
     setName("");
+    fetchCategories();
   }
 
   return (
@@ -23,10 +33,36 @@ export default function Categories() {
           onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
+        <select
+          className="mb-0"
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value="0">No parent categories</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
       </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr>
+                <td>{category.name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
